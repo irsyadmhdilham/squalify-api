a241f3a5d5fd6d6ff48df0188ebe490ecdf7566b
irsyadmhdilham <irsyadmhdilham@gmail.com>
2018-12-03 21:17:53 +0800

    update

--- apis/v1/profile/urls.py
+++ apis/v1/profile/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.urls import include
-from .views import ProfileDetail
+from .views import ProfileDetail, PushNotification, EmailNotification
 
 app_name = 'profile'
 
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^(?P<user_pk>\d+)/point/', include('apis.v1.point.urls', namespace='point')),
     url(r'^(?P<user_pk>\d+)/sales/', include('apis.v1.sales.urls', namespace='sales')),
     url(r'^(?P<user_pk>\d+)/group/', include('apis.v1.group.urls', namespace='group')),
+    url(r'^(?P<pk>\d+)/settings/push-notifications/$', PushNotification.as_view(), name='push-notifications'),
+    url(r'^(?P<pk>\d+)/settings/email-notification/$', EmailNotification.as_view(), name='email-notification'),
 ]
--- apis/v1/profile/views.py
+++ apis/v1/profile/views.py
@@ -1,7 +1,49 @@
 from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from apis._models.profile import Profile
 from .serializers import ProfileSerializer
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+class PushNotification(APIView):
+
+    def get(self, request, pk):
+        settings = Profile.objects.get(pk=pk).settings['notifications']['push_notification']
+        return Response(settings, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        profile = Profile.objects.get(pk=pk)
+        settings = profile.settings
+        subject = request.data.get('subject')
+        value = request.data.get('value')
+        try:
+            push = settings['notifications']['push_notification']
+            if subject in push:
+                push[subject] = value
+            profile.save()
+            data = settings['notifications']['push_notification']
+            return Response({'Succeed': True, 'data': data}, status=status.HTTP_200_OK)
+        except ValueError:
+            return Response({'Succeed': False}, status=status.HTTP_400_BAD_REQUEST)
+
+class EmailNotification(APIView):
+
+    def get(self, request, pk):
+        settings = Profile.objects.get(pk=pk).settings['notifications']
+        return Response(settings, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        profile = Profile.objects.get(pk=pk)
+        settings = profile.settings
+        value = request.data.get('value')
+        try:
+            notif = settings['notifications']
+            notif['email_notification'] = value
+            profile.save()
+            return Response({'Succeed': True}, status=status.HTTP_200_OK)
+        except ValueError:
+            return Response({'Succeed': False}, status=status.HTTP_400_BAD_REQUEST)
+
