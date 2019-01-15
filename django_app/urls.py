748f56d2f5e6c67ba2735f2adc560686cd6dd8a4
irsyadmhdilham <irsyadmhdilham@gmail.com>
2019-01-15 12:44:24 +0800

    update

--- django_app/urls.py
+++ django_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^account/', include('account.urls', namespace='account'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
