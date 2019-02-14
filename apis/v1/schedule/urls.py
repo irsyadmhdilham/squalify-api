40caa58f09d3bcc79436386f2046ff87ef8265ca
irsyadmhdilham <irsyadmhdilham@gmail.com>
2019-02-14 09:15:19 +0800

    update

--- apis/v1/schedule/urls.py
+++ apis/v1/schedule/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import ScheduleList, ScheduleDetail
+from .views import ScheduleList, ScheduleDetail, SchedulesFilter
 
 app_name = 'schedule'
 
 urlpatterns = [
     url(r'^$', ScheduleList.as_view(), name='schedule-list'),
-    url(r'^(?P<pk>\d+)/$', ScheduleDetail.as_view(), name='schedule-detail')
+    url(r'^(?P<pk>\d+)/$', ScheduleDetail.as_view(), name='schedule-detail'),
+    url(r'^filter/$', SchedulesFilter.as_view(), name='schedule-filter'),
 ]
--- apis/v1/schedule/views.py
+++ apis/v1/schedule/views.py
@@ -3,6 +3,8 @@ from rest_framework.authentication import TokenAuthentication, SessionAuthentica
 from .. ._models.profile import Profile
 from .. ._models.schedule import Schedule
 from .serializers import SchedulesSerializer
+from dateutil import parser
+from django.db.models import Q
 
 async def task(num):
     await asyncio.sleep(1)
@@ -33,3 +35,97 @@ class ScheduleDetail(generics.RetrieveUpdateDestroyAPIView):
         profile = Profile.objects.get(pk=user_pk)
         profile.schedules.remove(instance)
         instance.delete()
+
+class SchedulesFilter(generics.ListAPIView):
+    authentication_classes = (TokenAuthentication, SessionAuthentication,)
+    serializer_class = SchedulesSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('user_pk')
+        schedules = Profile.objects.get(pk=pk).schedules
+        title = self.request.query_params.get('t')
+        remark = self.request.query_params.get('r')
+        location = self.request.query_params.get('l')
+        f = self.request.query_params.get('f')
+        u = self.request.query_params.get('u')
+
+        result = schedules.order_by('-date')
+
+        date_from = None
+        date_until = None
+        if f != 'null':
+            date_from = parser.parse(f)
+        if u != 'null':
+            date_until = parser.parse(u)
+        
+        if title != 'undefined' and remark != 'undefined' and location != 'undefined' and date_from is not None and date_until is not None:
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(remark__icontains=remark) &
+                Q(location__icontains=location) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif title != 'undefined' and remark != 'undefined' and location != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(remark__icontains=remark) &
+                Q(location__icontains=location)
+            )
+        elif date_from is not None and date_until is not None and remark != 'undefined' and location != 'undefined':
+            result = schedules.filter(
+                Q(remark__icontains=remark) &
+                Q(location__icontains=location) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif date_from is not None and date_until is not None and title != 'undefined' and remark != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(remark__icontains=remark) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif date_from is not None and date_until is not None and title != 'undefined' and location != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(location__icontains=location) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif date_from is not None and date_until is not None and title != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif date_from is not None and date_until is not None and location != 'undefined':
+            result = schedules.filter(
+                Q(location__icontains=location) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif date_from is not None and date_until is not None and remark != 'undefined':
+            result = schedules.filter(
+                Q(remark__icontains=remark) &
+                Q(date__range=(date_from, date_until))
+            )
+        elif location != 'undefined' and remark != 'undefined':
+            result = schedules.filter(
+                Q(remark__icontains=remark) &
+                Q(location__icontains=location)
+            )
+        elif location != 'undefined' and title != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(location__icontains=location)
+            )
+        elif remark != 'undefined' and title != 'undefined':
+            result = schedules.filter(
+                Q(title__icontains=title) &
+                Q(remark__icontains=remark)
+            )
+        elif remark != 'undefined':
+            result = schedules.filter(remark__icontains=remark)
+        elif location != 'undefined':
+            result = schedules.filter(location__icontains=location)
+        elif title != 'undefined':
+            result = schedules.filter(title__icontains=title)
+        elif date_from is not None and date_until is not None:
+            result = schedules.filter(date__range=(date_from, date_until))
+
+        return result
